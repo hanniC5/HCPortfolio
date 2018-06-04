9deb212ec5db685b60681c8aa0ee5e6e6edffc3d
Hannah Crump <hcrump5@gmail.com>
2018-06-04 00:30:53 -0400

    responsive styles mostly done

--- dev/scripts/site.js
+++ dev/scripts/site.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $('.wrapper.title').fadeIn();
     $('button.hamburger').on('click', () => {
         console.log('button clicked');
         $(this).toggleClass('is-active');
