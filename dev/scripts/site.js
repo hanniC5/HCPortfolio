c427496f27b9f9af7d1047d4ad61f9c25cf35831
Hannah Crump <hcrump5@gmail.com>
2018-06-04 09:54:32 -0400

    smooth scroll added

--- dev/scripts/site.js
+++ dev/scripts/site.js
@@ -1,5 +1,13 @@
-$(document).ready(function () {
-    $('.wrapper.title').fadeIn();
+const sScroll = function () {
+    console.log('this is from site.js');
+    $('a').smoothScroll({
+        speed: 600
+    });
+}
+
+$(function() {
+    sScroll();
+    $('.title').fadeIn('slow');
     $('button.hamburger').on('click', () => {
         console.log('button clicked');
         $(this).toggleClass('is-active');
