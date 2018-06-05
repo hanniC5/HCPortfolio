9dcac4e199086eb39d4f5f23ef39b01b31fd52e5
Hannah Crump <hcrump5@gmail.com>
2018-06-05 15:44:27 -0400

    mobile responsive fixed

--- dev/scripts/site.js
+++ dev/scripts/site.js
@@ -1,23 +1,13 @@
 const sScroll = function() {
-    console.log('this is from site.js');
+    //console.log('this is from site.js');
     $('a').smoothScroll({
-        speed: 500
-    });
-}
-const displayHamNav = function() {
-    $('button.hamburger.hamburger--squeeze').on('click', () => {
-        $('button.hamburger').toggleClass('is-active');
-        $('nav.homeNav').toggleClass('display');
-    });
-    $('a.homeLink').on('click', () => {
-        $('button.hamburger').removeClass('is-active');
-        $('nav.homeNav').removeClass('display');
+        speed: 700
     });
 }
 
 $(function() {
-    sScroll();
-    $('div.title').fadeIn('slow');
-    displayHamNav();
-    
+    sScroll(); 
+    AOS.init({
+        duration: 1200
+    });   
 });
